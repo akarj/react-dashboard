68e3365b61c3a80f739ce915ca14a02173e3740e
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2022-07-23 00:47:58 +0530

    navbar was complated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,60 @@
+import {
+  ChatBubbleOutlineOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  LanguageOutlined,
+  ListOutlined,
+  NotificationsNoneOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
 import "./Navbar.scss";
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input
+            type="text"
+            className="form-control search"
+            placeholder="Search..."
+            id="text-search"
+          />
+          <SearchOutlined />
+        </div>
+
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined id="language" />
+            <label htmlFor="language">English</label>
+          </div>
+          <div className="item">
+            <DarkModeOutlined />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlined />
+          </div>
+          <div className="item">
+            <img
+              src="https://source.unsplash.com/400x400"
+              alt="user profile"
+              className="user-profile"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
-      home
+      <div className="homeContainer">
+        <Navbar />
+        container
+      </div>
     </div>
   );
 };
